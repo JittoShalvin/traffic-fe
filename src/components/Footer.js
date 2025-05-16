76cfee24ee1c4bf5b02d3e0b7426d97c5696441d
JittoShalvin <144763234+JittoShalvin@users.noreply.github.com>
2025-05-16 23:55:02 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,8 +25,7 @@ const Footer = () => {
           
           <div className="footer-map-container">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29550.900761537232!2d80.18698691132968!3d13.089941333111025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5264078822719b%3A0xbda01077b89581e2!2sAnna%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu!5e1!3m2!1sen!2sin!4v1743923494720!5m2!1sen!2sin" 
-              width="100%" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15800.591342781148!2d77.54587045000001!3d8.086402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04ed3d2a087861%3A0x1e790e896aeffaa0!2sKanniyakumari%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1747419791347!5m2!1sen!2sin" 
               height="300" 
               style={{border:0}} 
               allowFullScreen="" 
